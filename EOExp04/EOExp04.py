558fde18e0fc3e372740857227c29420ebb3bda4
behavian <chaoshaowei@gmail.com>
2021-04-23 23:04:29 +0800

    UPDATE EOExp04 06

--- EOExp04/EOExp04.py
+++ EOExp04/EOExp04.py
@@ -6,16 +6,16 @@ import math
 
 # File stuffs
 working_dir = (os.path.dirname(os.path.realpath(__file__)))
-filenames = ['EOExp06-1.csv', 'EOExp06-2.csv', 'EOExp06-3.csv']
-titles = ['measured power to angle difference of Linear polarization', 'measured power to angle difference', 'measured power to applied voltage']
-xlabels = [r'Angle difference $\rm{(deg)}$', r'Angle difference $\rm{(deg)}$', r'Applied voltage $\rm{(V)}$']
-ylabels = [r'Measured power $\rm{(mw)}$', r'Measured power $\rm{(mw)}$', r'Measured power $\rm{(mw)}$']
-legends = [['Measured'], [r'$\lambda/2$', r'$\lambda/4$'], ['Normal white liquid crystal']]
-major_locators = [90, 90, 1]
-minor_locators = [10, 45, 1]
-xmins = [0, 0, 0]
-xmaxs = [360, 360, 7]
-formatters = [r'{x:.0f}$^\circ$', r'{x:.0f}$^\circ$', r'{x:.0f}']
+filenames = ['EOExp04-1.csv']
+titles = ['measured power to voltage']
+xlabels = [r'Applied voltage $\rm{(V)}$']
+ylabels = [r'Measured power $\rm{(\mu w)}$']
+legends = [['Measured powe']]
+major_locators = [4]
+minor_locators = [1]
+xmins = [-12]
+xmaxs = [12]
+formatters = ['{x:.0f}']
 
 # plt stuffs
 figs = []
@@ -25,7 +25,7 @@ colors = ['crimson', 'mediumpurple', 'orchid']
 # Axes parameters
 left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
 
-for i in range(3):
+for i in range(1):
     y_datas = []
     figs.append(plt.figure(figsize=(12, 6)))
     axes.append(figs[i].add_axes([left, bottom, width, height]))
@@ -44,11 +44,6 @@ for i in range(3):
     axes[i].xaxis.set_minor_locator(MultipleLocator(minor_locators[i]))
     axes[i].set_xlim(xmin=xmins[i], xmax=xmaxs[i])
     axes[i].xaxis.set_major_formatter(formatters[i])
-
-axes[0].plot(range(361), [820*math.cos(i*math.pi/180)**2 for i in range(361)],
-    label='Theory', color=colors[1], linestyle='-.')
-
-for i in range(3):
     axes[i].legend(loc='upper right')
     figs[i].savefig(os.path.join(working_dir, f'fig{i+1}.png'))
 
