ad29afc409a1fa522bd054e19a286ed153b551bc
behavian <chaoshaowei@gmail.com>
2021-04-29 19:03:09 +0800

    UPDATE Exp05 Fit curve

--- EOExp05/EOExp05.py
+++ EOExp05/EOExp05.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import numpy as np
+import scipy
+import scipy.optimize
 import os
 import math
 
@@ -16,10 +18,10 @@ titles = ['Spectrum of Crystal LED', 'Spectrum of Market LED', 'IV Curve of Crys
 xlabels = [r'Frequency $\lambda\rm{(nm)}$', r'Frequency $\lambda\rm{(nm)}$', r'Voltage $\rm{(V)}$', r'Voltage $\rm{(V)}$']
 ylabels = [r'Intensity', r'Intensity', r'Current $\rm{(A)}$', r'Current $\rm{(A)}$']
 legends = [[fr'${i}\rm{{(mA)}}$' for i in range(10, 110, 10)], [fr'${i}\rm{{(mA)}}$' for i in range(1, 11)], ['Crystal LED'], ['Market LED']]
-major_locators = [50, 50, 1, 1]
-minor_locators = [50, 50, 0.2, 0.2]
-xmins = [350, 350, 0, 0]
-xmaxs = [750, 750, 3.6, 3.6]
+major_locators = [50, 50, 0.2, 0.2]
+minor_locators = [50, 50, 0.1, 0.1]
+xmins = [350, 350, 2, 2]
+xmaxs = [750, 750, 3.4, 2.9]
 formatters = ['{x:.0f}', '{x:.0f}', '{x:.1f}', '{x:.1f}']
 skip_headers = [19, 19, 2, 2]
 skip_footers = [1, 1, 0, 0]
@@ -43,9 +45,19 @@ for i in range(len(filenames)):
 
     for j in range(len(filenames[i])):
         my_data = np.genfromtxt(os.path.join(working_dir, filenames[i][j]), delimiter='\t', skip_header=skip_headers[i], skip_footer=skip_footers[i])
-        x_datas = [d[0] for d in my_data]
+        x_data = [d[0] for d in my_data]
         y_data = [d[1] for d in my_data]
-        axes[i].plot(x_datas, y_data, label=legends[i][j], color=colors[j])
+        axes[i].plot(x_data, y_data, label=legends[i][j], color=colors[j])
+        
+    if i >= 2:
+        fit = scipy.optimize.curve_fit(lambda t,a,b: a+b*t,  x_data[1000:],  y_data[1000:],  p0=(1, 0.1))
+        print(f' {fit[0][0]:.4f} + {fit[0][1]:.4f} * v')
+        axes[i].plot([i for i in np.arange(x_data[1000], x_data[-1], 0.01)], [fit[0][0]+fit[0][1]*i for i in np.arange(x_data[1000], x_data[-1], 0.01)], label=r'fit curve for $R_{S}$', color=colors[5])
+
+        fit = scipy.optimize.curve_fit(lambda t,a,b: a+b*t,  x_data[3:150],  [np.log(y) for y in y_data[3:150]],  p0=(1, 0.1))
+        print(f'exp ( {fit[0][0]:.4f} + {fit[0][1]:.4f} * v )')
+        axes[i].plot([i for i in np.arange(x_data[3], x_data[150], 0.01)], [np.exp(fit[0][0]+fit[0][1]*i) for i in np.arange(x_data[3], x_data[150], 0.01)], label=r'fit curve for $n$', color=colors[9])
+        axes[i].plot([i for i in np.arange(x_data[0], x_data[400], 0.01)], [np.exp(fit[0][0]+fit[0][1]*i) for i in np.arange(x_data[0], x_data[400], 0.01)], linestyle='--', color=colors[9])
         
     axes[i].xaxis.set_major_locator(MultipleLocator(major_locators[i]))
     axes[i].xaxis.set_minor_locator(MultipleLocator(minor_locators[i]))
